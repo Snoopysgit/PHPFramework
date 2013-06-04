e5f61b7935166551d778e442a6ba05a4b9ae19a1
Christian Thommen <christian@thommen.de>
2013-06-05 00:04:03 +0200

    comments

--- classes/components/ihtmlconpoment.interface.php
+++ classes/components/ihtmlconpoment.interface.php
@@ -2,11 +2,23 @@
 /**
  * Interface IHTMLComponent
  * Interface for all HTMLComponents. A HTML component is basicly a tag with content. Therefore the interface provides methods to get these tag information.
- * A Tag can be <a></a> or <div></div>
+ * A Tag can be {<a></a> or <div></div>}
+ * @author Christian Thommen
  */
 interface IHTMLComponent{
-	
+	/**
+	 * Gets the HTML of this component
+	 * 
+	 * This includes the content between the start and the end tag. e.g: 
+	 * {<div>Hello world!</div>}
+	 */
 	function getHTML();
+	/**
+	 * Gets the inner HTML of this component
+	 * 
+	 * This is just the content between the start and the end tag, but without the tags. e.g: 
+	 * {Hello world!}
+	 */
 	function getInnerHTML();
 }
 ?>
